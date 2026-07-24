defa2f2787eb715cc1e1c320b220a6a1d6c9f905
trajrealm <trajrealm@trajrealm.com>
2026-07-24 11:38:10 -0400

    add headers config for iframe embedding support

--- next.config.js
+++ next.config.js
@@ -4,6 +4,27 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  headers: async () => {
+    return [
+      {
+        source: '/embed',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: 'frame-ancestors *'
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
